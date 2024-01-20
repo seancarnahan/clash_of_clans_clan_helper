4d678d39e376856473fb1e0f8ba190830d642d2a
Sean Carnahan <fundmaker511@gmail.com>
2024-01-19 19:11:08 -0500

    main - hover logic done

--- lib/presentation/screens/clan_dashboard/widgets/clan_analysis_tool_dataset_option.dart
+++ lib/presentation/screens/clan_dashboard/widgets/clan_analysis_tool_dataset_option.dart
@@ -4,34 +4,47 @@ import 'package:provider/provider.dart';
 import 'package:clash_of_clans_clan_helper/presentation/providers/clan_analysis_tool_provider.dart';
 import 'package:clash_of_clans_clan_helper/presentation/models/dataset_analyzable.dart';
 
-// onHover -> change color to grey, and update elevation
-class ClanAnalysisToolDatasetOption extends StatelessWidget {
+class ClanAnalysisToolDatasetOption extends StatefulWidget {
   final DatasetAnalyzable dataset;
-  
+
   const ClanAnalysisToolDatasetOption({Key? key, required this.dataset}) : super(key: key);
 
+  @override
+  State<ClanAnalysisToolDatasetOption> createState() => _ClanAnalysisToolDatasetOptionState();
+}
+
+class _ClanAnalysisToolDatasetOptionState extends State<ClanAnalysisToolDatasetOption> {
+  bool isHovered = false;
+  
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double boxHeight = screenHeight * 0.2;
+    double boxHeightHovered = screenHeight * 0.3;
 
     return Consumer<ClanAnalysisToolProvider>(
       builder: (context, clanAnalysisToolProvider, child) {
-        bool isSelected = clanAnalysisToolProvider.selectedDataset == dataset.datasetName;
+        bool isSelected = clanAnalysisToolProvider.selectedDataset == widget.dataset.datasetName;
         Color? containerColor = isSelected ? Colors.blueGrey : null;
         Color textColor = isSelected ? Colors.white : Colors.black;
         Color iconColor = isSelected ? Colors.white : Colors.blueGrey;
 
         return MouseRegion(
+          onEnter: (_) => setState(() => isHovered = true),
+          onExit: (_) => setState(() => isHovered = false),
           cursor: SystemMouseCursors.click,
           child: GestureDetector(
-            onTap: () => clanAnalysisToolProvider.selectDataset(dataset.datasetName),
+            onTap: () => clanAnalysisToolProvider.selectDataset(widget.dataset.datasetName),
             child: Card(
-              child: Container(
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 100),
                 decoration: BoxDecoration(
                   color: containerColor,
                   border: Border.all(color: Colors.black, width: 1),
                   borderRadius: const BorderRadius.all(Radius.circular(20)),
+                  boxShadow: isHovered
+                  ? [BoxShadow(color: Colors.white, blurRadius: 10, spreadRadius: 3)]
+                  : [],
                 ),
                 height: boxHeight,
                 child: Padding(
@@ -40,9 +53,9 @@ class ClanAnalysisToolDatasetOption extends StatelessWidget {
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text(dataset.title, style: TextStyle(color: textColor)),
+                        Text(widget.dataset.title, style: TextStyle(color: textColor)),
                         const SizedBox(height: 16),
-                        Icon(dataset.icon, color: iconColor, size: 48),
+                        Icon(widget.dataset.icon, color: iconColor, size: 48),
                       ],
                     ),
                   ),
